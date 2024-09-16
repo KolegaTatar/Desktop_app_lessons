bfe75685204ec0cf4ee50274cf73e088dd5f4f33
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-09-16 21:05:41 +0200

    lesson file w

--- lesson1/funkcja_kwadratowa.cpp
+++ lesson1/funkcja_kwadratowa.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
 #include <cmath>
-#include <string.h>
+
 using namespace std;
 
-string funkcja_kwad(double a, double b, double c){
-    double delta =(b*b) - (4*a*c);
-    string message="";
-    if(delta<0){
-        message ="Brak pierwiastków";
+int solveQuadratic(double a, double b, double c, double* x1, double* x2) {
+    if (a == 0) {
+        return -1;
     }
-    if(delta ==0){
-        double x0 = ((-1)*b)/(2*a);
-        string x0s = to_string(x0);
-        message = "jeden pierwiastek"<x0s;
+    
+    double delta = b * b - 4 * a * c;
+    
+    if (delta > 0) {
+        
+        double sqrtDelta = sqrt(delta);
+        *x1 = (-b + sqrtDelta) / (2 * a);
+        *x2 = (-b - sqrtDelta) / (2 * a);
+        return 0;
+    } 
+	else if (delta == 0) {
+        *x1 = -b / (2 * a);
+        *x2 = *x1; 
+        return 0;
+    } 
+	else {
+        return -2;
     }
-    else{
-        double x1 = (((-1)*b)-sqrt(delta))/(2*a);
-        double x2 = (((-1)*b)+sqrt(delta))/(2*a);;
-        message = "x1 wynosi: "<<to_string(x1)<<"/n  x2 wynosi: "<<to_string(x2);
-    }
-	return message;
+}
 
+int main() {
+    double a, b, c;
+
+    cout << "Podaj wspolczynniki a, b i c (oddzielone spacjami): ";
+    cin >> a >> b >> c;
+
+    double x1, x2;
+    int result = solveQuadratic(a, b, c, &x1, &x2);
+    
+    switch (result) {
+        case 0:
+            if (x1 == x2) {
+                cout << "Jedyny pierwiastek: x = " << x1 << endl;
+            } 
+			else {
+                cout << "Pierwiastki: x1 = " << x1 << " oraz x2 = " << x2 << endl;
+            }
+            break;
+        case -1:
+            cout << "Blad: Wspolczynnik 'a' nie moze byc zerem w rownaniu kwadratowym." << endl;
+            break;
+        case -2:
+            cout << "Rownanie nie ma pierwiastkow rzeczywistych." <<endl;
+            break;
+    }
+    
+    return 0;
 }
-int main(){
-    cout<<funkcja_kwad(3.0,3.0,-1.0);
-	return 0;
-}
+
--- lesson1/pole trojkata - 3 wierzcholki.cpp
+++ lesson1/pole trojkata - 3 wierzcholki.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <cmath>
-#include <string.h>
+#include <cmath> 
+
 using namespace std;
 
-int main(){
-
-	return 0;
+int calculateTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3, double* area) {
+    if (area == NULL) {
+        
+        return -1;
+    }  
+    *area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+    return 0;
 }
+
+int main() {
+    double x1, y1, x2, y2, x3, y3;
+    
+    
+    cout << "Podaj wspolrzedne wierzcholkow trojkata (x1 y1 x2 y2 x3 y3): ";
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    
+    double area;
+    int result = calculateTriangleArea(x1, y1, x2, y2, x3, y3, &area);
+    
+    if (result == 0) {
+        cout << "Pole trojkata wynosi: " << area << endl;
+    } 
+	else {
+        cout << "Blad: Null" <<endl;
+    }
+    
+    return 0;
+}
+
